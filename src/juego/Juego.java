0a0eddd2b27fee56ae3d4b38fea77c9cb19ce649
ginoluraschi <gino.luraschi@outlook.com.ar>
2017-10-23 17:51:04 -0300

    Acomodado de codigo

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,3 +1,13 @@
+
+// ...............................................................................................................................
+//
+// (C) Copyright  2011/2017 TekGenesis.  All Rights Reserved
+// THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF TekGenesis.
+// The copyright notice above does not evidence any actual or intended
+// publication of such source code.
+//
+// ...............................................................................................................................
+
 package juego;
 
 // ...............................................................................................................................
@@ -13,251 +23,231 @@ package juego;
 import elementos.Barril;
 import elementos.Escalera;
 import elementos.Viga;
+
 import entorno.Entorno;
 import entorno.InterfaceJuego;
+
 import personajes.Agente;
 import personajes.Monkey;
 
 public class Juego extends InterfaceJuego {
 
-	// ~ Instance Fields
-	// ..............................................................................................................................
+    //~ Instance Fields ..............................................................................................................................
 
-	// Variables y métodos propios de cada grupo
-	// Personajes
-	Agente agente;
-	Monkey monkey;
+    // Variables y métodos propios de cada grupo
+    // Personajes
+    Agente agente;
 
-	// Elementos
-	Barril barril;
-	Viga vigas[];
-	Escalera escaleras[];
+    int alturaSalto = 0;
 
-	// El objeto Entorno que controla el tiempo y otros
-	private Entorno entorno;
-	private int CANT_VIGAS = 5;
+    // Elementos
+    Barril     barril;
+    Escalera[] escaleras;
+    Monkey     monkey;
+    boolean    saltando       = false;
+    boolean    saltandoArriba = true;
+    Viga[]     vigas;
 
-	// Variables y métodos propios de cada grupo
-	// ...
+    boolean volver = true;
 
-	Juego() {
-		// Inicializa el objeto entorno
-		this.entorno = new Entorno(this, "Donkey - Gino Luraschi - V0.01", ANCHO_FRAME, ALTO_FRAME);
+    boolean           volverBarril = false;
+    private final int CANT_VIGAS   = 5;
 
-		// Inicializar lo que haga falta para el juego
-		// Iniciamos personajes
-		monkey = new Monkey(50, 50);
-		iniciarAgente();
+    // El objeto Entorno que controla el tiempo y otros
+    private final Entorno entorno;
 
-		// Iniciamos elementos
-		iniciarVigas();
-		iniciarEscaleras();
+    //~ Constructors .................................................................................................................................
 
-		// Inicia el juego!
-		this.entorno.iniciar();
-	}
+    // Variables y métodos propios de cada grupo
+    // ...
 
-	private void iniciarAgente() {
-		agente = new Agente(50, ALTO_FRAME - 50);
-	}
+    Juego() {
+        // Inicializa el objeto entorno
+        entorno = new Entorno(this, "Donkey - Gino Luraschi - V0.01", ANCHO_FRAME, ALTO_FRAME);
 
-	private void iniciarEscaleras() {
-		escaleras = new Escalera[CANT_VIGAS - 1];// Se tiene una escalera menos que que vigas
-		for (int x = 0; x < escaleras.length; x++) {
-			escaleras[x] = ubicarEscalera(vigas[x], vigas[x + 1]);
-		}
-	}
+        // Inicializar lo que haga falta para el juego
+        // Iniciamos personajes
+        monkey = new Monkey(50, 50);
+        iniciarAgente();
 
-	private Escalera ubicarEscalera(Viga viga, Viga viga2) {
-		if (viga.getIzq()) {
-			int x = viga.getX() + viga.getAncho();
-			return new Escalera(x, viga.getY(), viga2.getY() - viga.getY());
-		}
-		int x = viga.getX();
-		return new Escalera(x, viga.getY(), viga2.getY() - viga.getY());
-	}
+        // Iniciamos elementos
+        iniciarVigas();
+        iniciarEscaleras();
 
-	private void iniciarVigas() {
-		vigas = new Viga[5];
-		vigas[0] = new Viga(0, monkey.primeraViga());
-		for (int x = 1; x < vigas.length; x++) {
-			int posX;
+        // Inicia el juego!
+        entorno.iniciar();
+    }
 
-			if (vigas[x - 1].getIzq())
-				posX = ANCHO_FRAME - Viga.ANCHO_DEFAULT;
-			else
-				posX = 0;
+    //~ Methods ......................................................................................................................................
 
-			int y = vigas[x - 1].getY() + ALTO_FRAME / (CANT_VIGAS);
-			vigas[x] = new Viga(posX, y);
-		}
-		vigas[CANT_VIGAS - 1].setAncho(ANCHO_FRAME);// Se tomara el piso
-	}
+    /**
+     * Durante el juego, el método tick() será ejecutado en cada instante y por lo tanto es el
+     * método más importante de esta clase. Aquí se debe actualizar el estado interno del juego para
+     * simular el paso del tiempo (ver el enunciado del TP para mayor detalle).
+     */
+    public void tick() {
+        // Procesamiento de un instante de tiempo
+        dibujarElementos();
+        moverPersonajes();
+        lanzarBarril();
+        verificarAgenteBarril();
+    }
 
-	// ~ Methods
-	// ......................................................................................................................................
+    private void dibujarElementos() {
+        dibujarVigas();
+        dibujarEscaleras();
+    }
 
-	/**
-	 * Durante el juego, el método tick() será ejecutado en cada instante y por lo
-	 * tanto es el método más importante de esta clase. Aquí se debe actualizar el
-	 * estado interno del juego para simular el paso del tiempo (ver el enunciado
-	 * del TP para mayor detalle).
-	 */
-	public void tick() {
-		// Procesamiento de un instante de tiempo
-		dibujarElementos();
-		moverPersonajes();
-		lanzarBarril();
-		verificarAgenteBarril();
-	}
+    private void dibujarEscaleras() {
+        for (final Escalera escalera : escaleras)
+            escalera.dibujarse(entorno);
+    }
 
-	private void verificarAgenteBarril() {
-		if (agente.lePega(barril)) {
-			iniciarAgente();
-			barril = null;
-		}
-	}
+    private void dibujarVigas() {
+        for (final Viga viga : vigas)
+            viga.dibujarse(entorno);
+    }
 
-	private void lanzarBarril() {
-		if (barril == null)
-			barril = monkey.lanzarBarril();
-		moverBarril();
-	}
+    private void iniciarAgente() {
+        agente = new Agente(50, ALTO_FRAME - 50);
+    }
 
-	boolean volverBarril = false;
-	boolean bajar = false;
+    private void iniciarEscaleras() {
+        escaleras = new Escalera[CANT_VIGAS - 1];  // Se tiene una escalera menos que que vigas
+        for (int x = 0; x < escaleras.length; x++)
+            escaleras[x] = ubicarEscalera(vigas[x], vigas[x + 1]);
+    }
 
-	private void moverBarril() {
-		boolean seMovio = false;
-		for (Viga viga : vigas) {
-			if (barril.verificarViga(viga)) {
-				// Tope derecho de viga
-				if (barril.llegoTopeDer(viga) && barril.tocoCostado(ANCHO_FRAME) && !volverBarril) {
-					volverBarril = true;
-				} else if (barril.llegoTopeDer(viga)) {
-					seMovio = false;
-				} else if (!barril.llegoTopeDer(viga) && !volverBarril) {
-					seMovio = true;
-					barril.moverse(2);
-					break;
-				}
+    private void iniciarVigas() {
+        vigas    = new Viga[5];
+        vigas[0] = new Viga(0, monkey.primeraViga());
+        for (int x = 1; x < vigas.length; x++) {
+            final int posX;
 
-				// Tope izquierdo de viga
-				if (barril.llegoTopeIzq(viga) && barril.tocoCostado(0) && volverBarril) {
-					volverBarril = false;
-				} else if (barril.llegoTopeIzq(viga)) {
-					seMovio = false;
-				} else if (!barril.llegoTopeIzq(viga) && volverBarril) {
-					seMovio = true;
-					barril.moverse(-2);
-					break;
-				}
-			}
-		}
-		if (!seMovio) {
-			barril.bajar();
-		}
-		barril.dibujarse(entorno);
-	}
+            if (vigas[x - 1].getIzq()) posX = ANCHO_FRAME - Viga.ANCHO_DEFAULT;
+            else posX = 0;
 
-	private void moverPersonajes() {
-		moverMonkey();
-		moverAgente();
-	}
+            final int y = vigas[x - 1].getY() + ALTO_FRAME / (CANT_VIGAS);
+            vigas[x] = new Viga(posX, y);
+        }
+        vigas[CANT_VIGAS - 1].setAncho(ANCHO_FRAME);  // Se tomara el piso
+    }
 
-	int alturaSalto = 0;
-	boolean saltando = false;
-	boolean saltandoArriba = true;
+    private void lanzarBarril() {
+        if (barril == null) barril = monkey.lanzarBarril();
+        moverBarril();
+    }
 
-	private void moverAgente() {
-		if (entorno.estaPresionada(entorno.TECLA_ARRIBA) || entorno.estaPresionada(entorno.TECLA_ABAJO)) {
-			for (Escalera escalera : escaleras) {
-				if (agente.verificarEscalera(escalera)) {
-					agente.moverPorEscalera(entorno, escalera);
-				}
-			}
-			agente.dibujarse(entorno);
-			return;
-		}
-		if (entorno.sePresiono(entorno.TECLA_ESPACIO) || saltando) {
-			if (!saltando) {
-				saltando = true;
-			}
+    private void moverAgente() {
+        if (entorno.estaPresionada(entorno.TECLA_ARRIBA) || entorno.estaPresionada(entorno.TECLA_ABAJO)) {
+            for (final Escalera escalera : escaleras) {
+                if (agente.verificarEscalera(escalera)) agente.moverPorEscalera(entorno, escalera);
+            }
+            agente.dibujarse(entorno);
+            return;
+        }
+        if (entorno.sePresiono(entorno.TECLA_ESPACIO) || saltando) {
+            if (!saltando) saltando = true;
 
-			if (saltando && alturaSalto < 60 && saltandoArriba) {
-				agente.subir(-1);
-				alturaSalto += 1;
-				if (alturaSalto >= 60) {
-					saltandoArriba = false;
-				}
-			}
-			if (saltando && !saltandoArriba) {
-				agente.subir(1);
-				alturaSalto -= 1;
-				if (alturaSalto <= 0) {
-					saltandoArriba = true;
-					saltando = false;
-					alturaSalto = 0;
-				}
-			}
-			agente.dibujarse(entorno);
-		} else {
-			for (Viga viga : vigas) {
-				if (agente.verificarViga(viga)) {
-					agente.moverPorViga(entorno, viga);
-				}
-				if(!saltando) ubicarYdibujarAgente();
-			}
-		}
-	}
+            if (alturaSalto < 60 && saltandoArriba) {
+                agente.subir(-1);
+                alturaSalto += 1;
+                if (alturaSalto >= 60) saltandoArriba = false;
+            }
+            if (saltando && !saltandoArriba) {
+                agente.subir(1);
+                alturaSalto -= 1;
+                if (alturaSalto <= 0) {
+                    saltandoArriba = true;
+                    saltando       = false;
+                    alturaSalto    = 0;
+                }
+            }
+            agente.dibujarse(entorno);
+        }
+        else {
+            for (final Viga viga : vigas) {
+                if (agente.verificarViga(viga)) agente.moverPorViga(entorno, viga);
+                if (!saltando) ubicarYdibujarAgente();
+            }
+        }
+    }
 
-	boolean volver = true;
+    private void moverBarril() {
+        boolean seMovio = false;
+        for (final Viga viga : vigas) {
+            if (barril.verificarViga(viga)) {
+                // Tope derecho de viga
+                if (barril.llegoTopeDer(viga) && barril.tocoCostado(ANCHO_FRAME) && !volverBarril) volverBarril = true;
+                else if (barril.llegoTopeDer(viga)) seMovio = false;
+                else if (!barril.llegoTopeDer(viga) && !volverBarril) {
+                    seMovio = true;
+                    barril.moverse(2);
+                    break;
+                }
 
-	private void moverMonkey() {
-		if (monkey.llegoTope(vigas[0]))
-			volver = !volver;
-		if (volver) // muevo a la izq
-			monkey.mover(-2);
-		else // Muevo a la derecha
-			monkey.mover(+2);
+                // Tope izquierdo de viga
+                if (barril.llegoTopeIzq(viga) && barril.tocoCostado(0) && volverBarril) volverBarril = false;
+                else if (barril.llegoTopeIzq(viga)) seMovio = false;
+                else if (!barril.llegoTopeIzq(viga) && volverBarril) {
+                    seMovio = true;
+                    barril.moverse(-2);
+                    break;
+                }
+            }
+        }
+        if (!seMovio) barril.bajar();
+        barril.dibujarse(entorno);
+    }
 
-		monkey.dibujarse(entorno);
-	}
+    private void moverMonkey() {
+        if (monkey.llegoTope(vigas[0])) volver = !volver;
+        if (volver)  // muevo a la izq
+            monkey.mover(-2);
+        else         // Muevo a la derecha
+            monkey.mover(+2);
 
-	private void dibujarElementos() {
-		dibujarVigas();
-		dibujarEscaleras();
-	}
+        monkey.dibujarse(entorno);
+    }
 
-	private void dibujarEscaleras() {
-		for (Escalera escalera : escaleras)
-			escalera.dibujarse(entorno);
-	}
+    private void moverPersonajes()
+    {
+        moverMonkey();
+        moverAgente();
+    }
 
-	private void ubicarYdibujarAgente() {
-		for (Viga viga : vigas) {
-			if (agente.verificarViga(viga))
-				agente.setY(viga.getY() - 15);
-		}
-		agente.dibujarse(entorno);
-	}
+    private Escalera ubicarEscalera(Viga viga, Viga viga2) {
+        if (viga.getIzq()) {
+            final int x = viga.getX() + viga.getAncho();
+            return new Escalera(x, viga.getY(), viga2.getY() - viga.getY());
+        }
+        final int x = viga.getX();
+        return new Escalera(x, viga.getY(), viga2.getY() - viga.getY());
+    }
 
-	private void dibujarVigas() {
-		for (Viga viga : vigas)
-			viga.dibujarse(entorno);
-	}
+    private void ubicarYdibujarAgente() {
+        for (final Viga viga : vigas) {
+            if (agente.verificarViga(viga)) agente.setY(viga.getY() - 15);
+        }
+        agente.dibujarse(entorno);
+    }
 
-	// ~ Methods
-	// ......................................................................................................................................
+    private void verificarAgenteBarril() {
+        if (agente.lePega(barril)) {
+            iniciarAgente();
+            barril = null;
+        }
+    }
 
-	@SuppressWarnings("unused")
-	public static void main(String[] args) {
-		Juego juego = new Juego();
-	}
+    //~ Methods ......................................................................................................................................
 
-	// ~ Static Fields
-	// ................................................................................................................................
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        final Juego juego = new Juego();
+    }
 
-	private static final int ALTO_FRAME = 600;
-	private static final int ANCHO_FRAME = 800;
-} // end class Juego
+    //~ Static Fields ................................................................................................................................
+
+    private static final int ALTO_FRAME  = 600;
+    private static final int ANCHO_FRAME = 800;
+}  // end class Juego
